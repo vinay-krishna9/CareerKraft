5b7db7854c3548b3c9d14d9cdd1d7e96eaf1001c
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2026-06-23 21:17:05 +0200

    feat(cover-letter): save and display personal info with store

--- careerkraft-web/src/app/features/cover-letters/pages/cover-letter-editor-page/cover-letter-editor-page.ts
+++ careerkraft-web/src/app/features/cover-letters/pages/cover-letter-editor-page/cover-letter-editor-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Store } from '@ngrx/store';
+import { selectCurrentCoverLetter } from '../../store/cover-letter.selectors';
 
 @Component({
   selector: 'app-cover-letter-editor-page',
@@ -7,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './cover-letter-editor-page.scss',
 })
 export class CoverLetterEditorPage {
+  store = inject(Store);
 
+  readonly currentCoverLetter = toSignal(this.store.select(selectCurrentCoverLetter), {
+    initialValue: null,
+  });
 }
--- careerkraft-web/src/app/features/cover-letters/pages/cover-letter-list-page/cover-letter-list-page.ts
+++ careerkraft-web/src/app/features/cover-letters/pages/cover-letter-list-page/cover-letter-list-page.ts
@@ -4,6 +4,9 @@ import { PersonalInfo } from '../../../../shared/models/personal-info.model';
 import { CoverLetter } from '../../cover-letter.model';
 import { PersonalInfoForm } from '../../../../shared/components/personal-info-form/personal-info-form';
 import { ModalService } from '../../../../shared/services/modal.service';
+import { Store } from '@ngrx/store';
+import { CoverLetterActions } from '../../store/cover-letter.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cover-letter-list-page',
@@ -13,8 +16,16 @@ import { ModalService } from '../../../../shared/services/modal.service';
 })
 export class CoverLetterListPage {
   modal = inject(ModalService);
+  store = inject(Store);
+  router = inject(Router);
 
+  // TODO: fetch cover letters and add id to route(TBD)
   readonly coverLetters: CoverLetter[] = [];
 
-  onPersonalInfoSubmitted(personalInfo: PersonalInfo): void {}
+  onPersonalInfoSubmitted(personalInfo: PersonalInfo): void {
+    this.store.dispatch(CoverLetterActions.createDraftCoverLetter({ personalInfo }));
+
+    // :id/edit (TBD)
+    this.router.navigate(['/cover-letters/new/edit']);
+  }
 }
