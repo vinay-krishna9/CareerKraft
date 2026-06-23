33f04b2fe73c3ab6fbf489f782c57af1aa1f512b
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2026-06-23 20:55:31 +0200

    feat(cover-letter): integrate personal info form as modal

--- careerkraft-web/src/app/features/cover-letters/pages/cover-letter-list-page/cover-letter-list-page.ts
+++ careerkraft-web/src/app/features/cover-letters/pages/cover-letter-list-page/cover-letter-list-page.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DocumentTile } from '../../../../shared/ui/document-tile/document-tile';
+import { PersonalInfo } from '../../../../shared/models/personal-info.model';
+import { CoverLetter } from '../../cover-letter.model';
+import { PersonalInfoForm } from '../../../../shared/components/personal-info-form/personal-info-form';
+import { ModalService } from '../../../../shared/services/modal.service';
 
 @Component({
   selector: 'app-cover-letter-list-page',
-  imports: [],
+  imports: [PersonalInfoForm, DocumentTile],
   templateUrl: './cover-letter-list-page.html',
   styleUrl: './cover-letter-list-page.scss',
 })
 export class CoverLetterListPage {
+  modal = inject(ModalService);
 
+  readonly coverLetters: CoverLetter[] = [];
+
+  onPersonalInfoSubmitted(personalInfo: PersonalInfo): void {}
 }
